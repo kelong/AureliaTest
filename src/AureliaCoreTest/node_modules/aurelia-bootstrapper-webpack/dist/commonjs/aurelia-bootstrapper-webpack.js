b2b64795ea1412e422624e24208f796c8d676a99
Maciej Lelito <maciej.lelito@me.com>
2017-02-12 17:08:08 +0100

    initial commit

---
+++ src/AureliaCoreTest/node_modules/aurelia-bootstrapper-webpack/dist/commonjs/aurelia-bootstrapper-webpack.js
@@ -0,0 +1,101 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.bootstrap = bootstrap;
+
+require('aurelia-polyfills');
+
+var _aureliaPalBrowser = require('aurelia-pal-browser');
+
+var _aureliaLoaderWebpack = require('aurelia-loader-webpack');
+
+(0, _aureliaPalBrowser.initialize)();
+
+var bootstrapQueue = [];
+var sharedLoader = null;
+var Aurelia = null;
+
+function onBootstrap(callback) {
+  return new Promise(function (resolve, reject) {
+    if (sharedLoader) {
+      resolve(callback(sharedLoader));
+    } else {
+      bootstrapQueue.push(function () {
+        try {
+          resolve(callback(sharedLoader));
+        } catch (e) {
+          reject(e);
+        }
+      });
+    }
+  });
+}
+
+function ready(global) {
+  return new Promise(function (resolve, reject) {
+    if (global.document.readyState === 'complete') {
+      resolve(global.document);
+    } else {
+      global.document.addEventListener('DOMContentLoaded', completed);
+      global.addEventListener('load', completed);
+    }
+
+    function completed() {
+      global.document.removeEventListener('DOMContentLoaded', completed);
+      global.removeEventListener('load', completed);
+      resolve(global.document);
+    }
+  });
+}
+
+function handleApp(loader, appHost) {
+  return config(loader, appHost, appHost.getAttribute('aurelia-app'));
+}
+
+function config(loader, appHost, configModuleId) {
+  var aurelia = new Aurelia(loader);
+  aurelia.host = appHost;
+  aurelia.configModuleId = configModuleId || null;
+
+  if (configModuleId) {
+    return loader.loadModule(configModuleId).then(function (customConfig) {
+      return customConfig.configure(aurelia);
+    });
+  }
+
+  aurelia.use.standardConfiguration().developmentLogging();
+
+  return aurelia.start().then(function () {
+    return aurelia.setRoot();
+  });
+}
+
+function run() {
+  return ready(window).then(function (doc) {
+    var appHost = doc.querySelectorAll('[aurelia-app]');
+    var loader = new _aureliaLoaderWebpack.WebpackLoader();
+    loader.loadModule('aurelia-framework').then(function (m) {
+      Aurelia = m.Aurelia;
+      for (var i = 0, ii = appHost.length; i < ii; ++i) {
+        handleApp(loader, appHost[i]).catch(console.error.bind(console));
+      }
+
+      sharedLoader = loader;
+      for (var _i = 0, _ii = bootstrapQueue.length; _i < _ii; ++_i) {
+        bootstrapQueue[_i]();
+      }
+      bootstrapQueue = null;
+    });
+  });
+}
+
+function bootstrap(configure) {
+  return onBootstrap(function (loader) {
+    var aurelia = new Aurelia(loader);
+    return configure(aurelia);
+  });
+}
+
+run();
